ac9c59c0fc118dbb7a58944e6584562fb8dd8082
Caleb Vredevoogd <cvredevoogd99@gmail.com>
2021-09-09 11:43:50 -0400

    Creates functions for current and historical purchase values

--- crypto-profit-tracker.py
+++ crypto-profit-tracker.py
@@ -2,54 +2,88 @@
 from pycoingecko import CoinGeckoAPI
 import csv
 
-with open('crypto.csv', 'r') as csvledger:
-    #Inside a csv file...
-    reader = csv.reader(csvledger,  skipinitialspace=True)
-    for row in reader:
-        #Coin name
-        id = row[0]
-        print("Coin name", id)
+def main():
+    with open('crypto.csv', 'r') as csvledger:
+        #Inside a csv file...
+        reader = csv.reader(csvledger,  skipinitialspace=True)
+        for row in reader:
+            #Coin name
+            id = row[0]
+            print("Coin name", id)
 
-        #Amount
-        amt = row[1]
-        print("Amount purchased:", amt)
+            #Amount
+            amt = row[1]
+            print("Amount purchased:", amt)
 
-        #Date purchased
-        dp = row[2]
-        print("Date purchased:", dp)
+            #Date purchased
+            dp = row[2]
+            print("Date purchased:", dp)
 
-        #Currency
-        cry = row[3]
-        print("Currency used:", cry)
+            #Currency
+            cry = row[3]
+            print("Currency used:", cry)
 
 
-        cg = CoinGeckoAPI()
-        cgdata = cg.get_coin_history_by_id(id=id, date=dp)
-        cg_md = cgdata['market_data']
-        price = cg_md['current_price']
-        
-        #Price of currency at date
-        price_cry_dt = price[cry] 
-        print("Currency Price at Date Purchased:", price_cry_dt)
+            cg = CoinGeckoAPI()
+            #Historical data
+            cgdatah = cg.get_coin_history_by_id(id=id, date=dp)
+            ms = return_money_spent(cgdatah, cry, amt)
+            print("Money spent:", ms)
 
-        #Money spent
-        ms = float(price_cry_dt) * float(amt)
-        print("Money spent:", ms)
+            #Current price calculation
+            #Current data
+            cgdatac=cg.get_price(ids=id, vs_currencies = cry)
+            
+            #current value of purchase
+            cvp = return_current_value(cgdatac, cry, amt)
+            
+            print("Current value of purchase:", cvp)
+            
+            #Change in value
+            cv = float(cvp) - ms
+            print("Change in value:", cv)
+            print()
+    
+'''
+return_money_spent
+@input: cgd - dictionary of data from CoinGecko, currency - string representing currency used in purchase
+@output: money_spent - float representing money spent on purchase
+'''
+def return_money_spent(cgd, currency, amount):
+    cg_md = cgd['market_data']
+    price = cg_md['current_price']
+    
+    #Price of currency at date
+    price_cry_dt = price[currency] 
+    print("Crypto Price at Date Purchased:", price_cry_dt)
 
-        #Current price calculation
-        cgdata=cg.get_price(ids=id, vs_currencies = cry)
-        print(cgdata)
-        cid = cgdata[id]
+    #Money spent
+    money_spent =  float(price_cry_dt) * float(amount)
+    return money_spent
 
-        #current price
-        cpr = cid[cry]
-        print("Current price:",cpr)
+'''
+return_current_value
+@input: cgd - dictionary of data from CoinGecko, currency - string representing currency used in purchase
+@output: money_spent - float representing current value of purchase
+'''
+def return_current_value(cgd, currency, amount):
+    
+    cid = cgd[id]
 
-        #current value or purchase
-        cvp = cpr * float(amt)
-        print("Current value of purchase:", cvp)
-        
-        #Change in value
-        cv = float(cvp) - ms
-        print("Change in value:", cv)
-        print()
+    #current price
+    cpr = cid[currency]
+    print("Current price:",cpr)
+
+    #current value of purchase
+    cvp = cpr * float(amount)
+
+    return cvp
+
+    
+    
+    
+    
+    
+
+    
+main()
